250adf4887a2acffa507973790f0d8550bc2eef0
bsindhu09 <166121070+bsindhu09@users.noreply.github.com>
2024-04-05 15:44:51 +0530

    Add files via upload

---
+++ String.sample.java
@@ -0,0 +1,46 @@
+class String.sample{
+    public static void main(String[]args){ 
+	   String s1="Krishna";
+	   String s2="Polytechnic";
+	   
+	   System.out.println("the string s1 is"+s1);
+	   System.out.println("the string s2 is"+s2);
+	   
+	   System.out.println("the length of string s1 is "+s1.length());
+	   System.out.println("the length of string s2 is "+s2.length());
+	   
+	   System.out.println("the uppercase of string s1 is"+s1.toUpperCase());
+	   System.out.println("the uppercase of string s2 is"+s2.toUpperCase());
+	   
+	   System.out.println("the lowercase of string s1 is"+s1.toLowerCase());
+	   System.out.println("the lowercase of string s2 is"+s2.toLowerCase());
+	   
+	   System.out.println("s1 is equal s2"+s1.equal(s2));
+	   System.out.println("comparing two string");
+	   
+	   int result=s1.compareTo(s2)
+	    if (result==0)
+		 {
+		 system.out.println("they are same");
+		 }
+		else if(result > 0)
+		 {
+		 system.out.println("s1 is greater than s2");
+		 }
+		else
+		{
+		   system.out.println("s1 is lesser than s2");
+		}
+	}
+}
+	   
+	   
+	   
+	   
+	   
+	   
+	
+	   
+	   
+	  
+	   
